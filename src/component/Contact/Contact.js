b8fbf63e221a6229b348374df9f95ad728be9837
Sasithorn Sangsanid <s5935512014@phuket.psu.ac.th>
2019-09-01 03:34:16 +0700

    newwwww

--- src/component/Contact/Contact.js
+++ src/component/Contact/Contact.js
@@ -7,7 +7,7 @@ class Contact extends Component {
       <div>
         <img 
           className="cont"
-          src="https://homebeco.com/wp-content/uploads/2019/03/contact_us_png_315841.png?w=1400" alt="pic">
+          src="https://scontent.fbkk8-3.fna.fbcdn.net/v/t1.0-9/69890288_2235316906595476_4818115315042877440_o.jpg?_nc_cat=111&_nc_oc=AQk7U0dYptZlHwEqErg8jxUFvTJJf3zHSZUK5GBPSyZQpDrduxfwxQayXOQWfEjun3U&_nc_ht=scontent.fbkk8-3.fna&oh=8aab4f632783892fd37420e28f1dee9f&oe=5E01979E" alt="pic">
             
           </img>
       </div>
