168b2071cb205899e67aa12f8602c25f4b02987c
“Pangkoki20” <sasithorn.pangkoki@gmail.com>
2019-10-25 13:46:49 +0700

    test

--- src/component/OrderConfirm/BtOder.js
+++ src/component/OrderConfirm/BtOder.js
@@ -1,34 +1,47 @@
 import React, { Component } from "react";
-import { Row, Col, Button } from "reactstrap";
+import { Row, Col, Button, Contrainer } from "reactstrap";
 import "./OrderConfirm.css";
 import Address from "./Address";
 class BtOrder extends Component {
   render() {
     return (
-      <div class="row">
-        <div class="col"></div>
-        <div class="col-md-auto">
-          <Button
-            outline
-            color="info"
-            size="lg"
-            block
-            className="btmodify_food"
-            href="/Basket"
-          >
-            แก้ไขรายการสั่งอาหาร
-          </Button>
-        </div>
-        <div class="col-md-auto">
-          <Button
-            color="info"
-            size="lg"
-            block
-            className="btmodify_food"
-            href="/Payment"
-          >
-            ต่อไป
-          </Button>
+      <div class="contrainer">
+        <div class="row from_btnextback">
+          <div class="col">
+            <Button
+              color="info"
+              size="lg"
+              block
+              className="btmodify_food"
+              href="/Address"
+            >
+              ย้อนกลับ
+            </Button>
+
+            <div class="col">
+              <Button
+                outline
+                color="info"
+                size="lg"
+                block
+                className="btmodify_food"
+                href="/Basket"
+              >
+                แก้ไขรายการสั่งอาหาร
+              </Button>
+            </div>
+            <div class="col">
+              <Button
+                color="info"
+                size="lg"
+                block
+                class="btmodify_food"
+                href="/Payment"
+              >
+                ต่อไป
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     );
--- src/component/OrderConfirm/Payment.js
+++ src/component/OrderConfirm/Payment.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { CardHeader, Card, CardBody, Button } from "reactstrap";
 import "./Payment.css";
 import Order from "./Oder";
+import RtOrder from "./BtOder";
 class Payment extends Component {
   render() {
     return (
@@ -15,6 +16,9 @@ class Payment extends Component {
             <CardHeader className="text_payment">วิธีการชำระเงิน</CardHeader>
             <CardBody className="bt_payment"></CardBody>
           </Card>
+          <div>
+            <RtOrder/>
+          </div>
         </div>
       </div>
     );
