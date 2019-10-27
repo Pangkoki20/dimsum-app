91aac356580c6fd42e753d6da08f7ed30f595525
“Pangkoki20” <sasithorn.pangkoki@gmail.com>
2019-10-28 02:06:35 +0700

    update_state_Menu

--- src/component/Menu/MenuNewEngpan.js
+++ src/component/Menu/MenuNewEngpan.js
@@ -5,6 +5,33 @@ import img_eggpan from "../../imgfastfoood/ไข่กะทะ.png";
 import BTBasket from "../BTBasket/BTBasket";
 
 class MenuNewEggpan extends Component {
+  state = { menu_name: "hargao", menu_value: 0, menu_price: 20 };
+
+  sentOrder = e => {
+    e.preventDefault();
+
+    var oldItems = JSON.parse(localStorage.getItem("order")) || [];
+
+    console.log("sent !");
+
+    const newData = {
+      menu_name: this.state.menu_name,
+      menu_value: 1,
+      menu_price: this.state.menu_price
+    };
+    oldItems.push(newData);
+    localStorage.setItem("order", JSON.stringify(oldItems));
+    console.log(" data : ", newData);
+    this.setState({ menu_value: 0 });
+    this.setState({ remark: "" });
+  };
+
+  handleInputChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+    this.setState({ please: "" });
+    console.log({ [name]: value });
+  };
   render() {
     return (
       <div>
--- src/component/Menu/MenuNewKanomgee.js
+++ src/component/Menu/MenuNewKanomgee.js
@@ -5,6 +5,33 @@ import img_kanomgee from "../../imgfastfoood/ขนมจีนน้ำแก�
 import BTBasket from "../BTBasket/BTBasket";
 
 class MenuNewKanomgee extends Component {
+  state = { menu_name: "hargao", menu_value: 0, menu_price: 20 };
+
+  sentOrder = e => {
+    e.preventDefault();
+
+    var oldItems = JSON.parse(localStorage.getItem("order")) || [];
+
+    console.log("sent !");
+
+    const newData = {
+      menu_name: this.state.menu_name,
+      menu_value: 1,
+      menu_price: this.state.menu_price
+    };
+    oldItems.push(newData);
+    localStorage.setItem("order", JSON.stringify(oldItems));
+    console.log(" data : ", newData);
+    this.setState({ menu_value: 0 });
+    this.setState({ remark: "" });
+  };
+
+  handleInputChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+    this.setState({ please: "" });
+    console.log({ [name]: value });
+  };
   render() {
     return (
       <div>
