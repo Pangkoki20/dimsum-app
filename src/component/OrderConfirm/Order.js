09e6491d20987d3e1504d8cc476fb912b09334e2
“Pangkoki20” <sasithorn.pangkoki@gmail.com>
2019-11-08 23:55:17 +0700

    state

--- src/component/OrderConfirm/Order.js
+++ src/component/OrderConfirm/Order.js
@@ -5,27 +5,35 @@ import img_address from "../../imgorder/address.png";
 import img_pay from "../../imgorder/pay.png";
 import img_confirm from "../../imgorder/confirm.png";
 class Order extends Component {
+  state = {
+    step: 1
+  };
+  onUserChanged = address => {
+    this.setState({ address });
+  };
   render() {
+    const { address } = this.state;
     return (
       <div class="container">
         <div class="card shadow-lg p-3 mb-5  bg-white rounded">
           <div className="text_basketconfirm">ยืนยันการสั่งซื้อ</div>
           <Row className="from_order">
-            <Col xs="6" sm="4">
-              <div>
-                <Button outline color="info" size="lg" block>
-                  <div>
-                    <img
-                      className="img_address"
-                      src={img_address}
-                      href="/Address"
-                    />
-                  </div>
-                  <div className="text-addre">ที่อยู่สำหรับการจัดส่ง</div>
-                </Button>
-              </div>
-            </Col>
-
+            {this.state.step == 1 && (
+              <Col xs="6" sm="4">
+                <div>
+                  <Button outline color="info" size="lg" block>
+                    <div>
+                      <img
+                        className="img_address"
+                        src={img_address}
+                        href="/Address"
+                      />
+                    </div>
+                    <div className="text-addre">ที่อยู่สำหรับการจัดส่ง</div>
+                  </Button>
+                </div>
+              </Col>
+            )}
             <Col xs="6" sm="4">
               <div>
                 <Button outline color="info" size="lg" block>
