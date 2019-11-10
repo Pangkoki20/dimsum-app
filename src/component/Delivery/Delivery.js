1c9600a8322520730dc2e32d7ee0560f96f577a6
“Pangkoki20” <sasithorn.pangkoki@gmail.com>
2019-11-11 02:06:15 +0700

    Delivery

--- src/component/Delivery/Delivery.js
+++ src/component/Delivery/Delivery.js
@@ -1,13 +1,43 @@
 import React, { Component } from "react";
+import {
+  Container,Card,
+  Button,
+  CardTitle,
+  CardText,
+  Row,
+  Col
+} from "reactstrap";
 import "./Delivery.css";
 
 class Delivery extends Component {
   render() {
     return (
-      <div>
-        สถานะการจัดส่ง
+      <div className="container-fluid">
+        <div className="text_status">สถานะการจัดส่ง</div>
+        <div className="from_status">
+          <Row>
+            <Col>
+              <Card body>
+                ทางร้านได้รับ Order ของคุณแล้ว
+              </Card>
+            </Col>
+            <Col>
+              <Card body>
+                กำลังจัดเตรียมอาหารของคุณ
+              </Card>
+            </Col>
+            <Col>
+              <Card body>อาหารของคุณพร้อมส่งถึงคุณ</Card>
+            </Col>
+            <Col>
+              <Card body>อาหารกำลังไปส่งถึงคุณ</Card>
+            </Col>
+            <Col>
+              <Card body>คุณได้รับอาหารแล้ว</Card>
+            </Col>
+          </Row>
+        </div>
       </div>
-      
     );
   }
 }
