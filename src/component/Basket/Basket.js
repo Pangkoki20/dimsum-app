9bc7e1a75d9b7324babb37f8c87311ce31d7ee9c
“Pangkoki20” <sasithorn.pangkoki@gmail.com>
2019-11-12 19:54:22 +0700

    Status_Order

--- src/component/Basket/Basket.js
+++ src/component/Basket/Basket.js
@@ -14,12 +14,13 @@ import {
 import "./Basket.css";
 import img_gps from "../../imgbasket/confirmation.png";
 import img_add from "../../imgbasket/plus.png";
+import MenuSelect from "../MenuSelect/MenuSelect";
 class Basket extends Component {
   state = {
     order: [],
     priceAll: 0,
     total: 0,
-    check:1
+    check: 1
   };
   componentDidMount = () => {
     let order = JSON.parse(localStorage.getItem("order"));
@@ -54,91 +55,98 @@ class Basket extends Component {
 
   render() {
     return (
-      <div class="container-fluid">
-        <div className="text_basket">อาหารในตะกร้า</div>
-        <div class="row">
-          <div class="col-12 col-md-8">
-            <Card className="card_food">
-              <CardHeader>
-                <Row>
-                  <Col>ลำดับที่</Col>
-                  <Col>ชื่ออาหาร</Col>
-                  <Col>ราคา</Col>
-                  {/* <Col>จำนวน</Col> */}
-                  <Col>ลบออก</Col>
-                </Row>
-              </CardHeader>
-              {this.state.order.map((e, index) => {
-                return (
-                  <div key={index + 1}>
-                    <CardBody>
-                      <Row>
-                        <Col> {index + 1}</Col>
-                        <Col>{e.menu_name} </Col>
-                        <Col> {e.menu_price}</Col>
-                        <Col className="img_trash">
-                          <div onClick={() => this.toRemove(index)}>
-                            <i class="far fa-trash-alt"></i>
-                          </div>
-                        </Col>
-                      </Row>
-                    </CardBody>
-                  </div>
-                );
-              })}
+      <div>
+        <MenuSelect/>
+        <div class="container-fluid">
+          <div className="text_basket">อาหารในตะกร้า</div>
+          <div class="row">
+            <div class="col-12 col-md-8">
+              <Card className="card_food">
+                <CardHeader>
+                  <Row>
+                    <Col>ลำดับที่</Col>
+                    <Col>ชื่ออาหาร</Col>
+                    <Col>ราคา</Col>
+                    {/* <Col>จำนวน</Col> */}
+                    <Col>ลบออก</Col>
+                  </Row>
+                </CardHeader>
+                {this.state.order.map((e, index) => {
+                  return (
+                    <div key={index + 1}>
+                      <CardBody>
+                        <Row>
+                          <Col> {index + 1}</Col>
+                          <Col>{e.menu_name} </Col>
+                          <Col> {e.menu_price}</Col>
+                          <Col className="img_trash">
+                            <div onClick={() => this.toRemove(index)}>
+                              <i class="far fa-trash-alt"></i>
+                            </div>
+                          </Col>
+                        </Row>
+                      </CardBody>
+                    </div>
+                  );
+                })}
 
-              <CardFooter>
-                <div class="row">
-                  <div class="col">จำนวนทั้งหมด</div>
-                  <div class="col col-lg-2">{this.state.order.length}</div>
-                  <div class="col col-lg-2">ชิ้น</div>
-                </div>
-              </CardFooter>
-            </Card>
-          </div>
-          <div class="col-6 col-md-4 ">
-            <div class="card from_money">
-              <Card>
-                <CardHeader className="text_money">รวมทั้งหมด</CardHeader>
-                <CardBody>
-                  <div className="d-flex">
-                    <div className="col-sm-8">
-                      <CardText className="tt_food">ราคาอาหารทั้งหมด</CardText>
-                    </div>
-                    <div className="col-sm-4">
-                      <CardText className="tt_food">
-                        {this.state.priceAll} บาท
-                      </CardText>
-                    </div>
-                  </div>
-
-                  <div className="d-flex">
-                    <div className="col-sm-8">
-                      <CardText className="delivery_charge">ค่าจัดส่ง</CardText>
-                    </div>
-                    <div className="col-sm-4">
-                      <CardText className="delivery_charge">30 บาท</CardText>
-                    </div>
-                  </div>
-                </CardBody>
-                <CardFooter className="tt_money">
+                <CardFooter>
                   <div class="row">
-                    <div class="col-sm-8"> รวม</div>
-                    <div class="col-sm-4">{this.state.priceAll + 30} </div>
+                    <div class="col">จำนวนทั้งหมด</div>
+                    <div class="col col-lg-2">{this.state.order.length}</div>
+                    <div class="col col-lg-2">ชิ้น</div>
                   </div>
                 </CardFooter>
               </Card>
             </div>
-            <div className="add_food">
-              <Button color="info" size="lg" block href="/Menu">
-                เพิ่มรายการอาหาร
-              </Button>
-            </div>
-            <div className="address_customer">
-              <Button outline color="info" size="lg" block href="/Order">
-                <img className="img_gps" src={img_gps} />
-                ยืนยันการสั่งซื้อ
-              </Button>
+            <div class="col-6 col-md-4 ">
+              <div class="card from_money">
+                <Card>
+                  <CardHeader className="text_money">รวมทั้งหมด</CardHeader>
+                  <CardBody>
+                    <div className="d-flex">
+                      <div className="col-sm-8">
+                        <CardText className="tt_food">
+                          ราคาอาหารทั้งหมด
+                        </CardText>
+                      </div>
+                      <div className="col-sm-4">
+                        <CardText className="tt_food">
+                          {this.state.priceAll} บาท
+                        </CardText>
+                      </div>
+                    </div>
+
+                    <div className="d-flex">
+                      <div className="col-sm-8">
+                        <CardText className="delivery_charge">
+                          ค่าจัดส่ง
+                        </CardText>
+                      </div>
+                      <div className="col-sm-4">
+                        <CardText className="delivery_charge">30 บาท</CardText>
+                      </div>
+                    </div>
+                  </CardBody>
+                  <CardFooter className="tt_money">
+                    <div class="row">
+                      <div class="col-sm-8"> รวม</div>
+                      <div class="col-sm-4">{this.state.priceAll + 30} </div>
+                    </div>
+                  </CardFooter>
+                </Card>
+              </div>
+              <div className="add_food">
+                <Button color="info" size="lg" block href="/Menu">
+                  เพิ่มรายการอาหาร
+                </Button>
+              </div>
+              <div className="address_customer">
+                <Button outline color="info" size="lg" block href="/Order">
+                  <img className="img_gps" src={img_gps} />
+                  ยืนยันการสั่งซื้อ
+                </Button>
+              </div>
             </div>
           </div>
         </div>
--- src/component/Delivery/Delivery.js
+++ src/component/Delivery/Delivery.js
@@ -1,41 +1,73 @@
 import React, { Component } from "react";
 import {
-  Container,Card,
-  Button,
-  CardTitle,
-  CardText,
+  Card,
   Row,
   Col
 } from "reactstrap";
 import "./Delivery.css";
-
+import MenuSelect from "../MenuSelect/MenuSelect";
+import img_menu from "../../imgdelivery/menu.png";
+import img_meal from "../../imgdelivery/meal.png";
+import img_product from "../../imgdelivery/product2.png";
+import img_delivery from "../../imgdelivery/delivery.png";
+import img_breakfast from "../../imgdelivery/breakfast.png";
 class Delivery extends Component {
   render() {
     return (
-      <div className="container-fluid">
-        <div className="text_status">สถานะการจัดส่ง</div>
-        <div className="from_status">
-          <Row>
-            <Col>
-              <Card body>
-                ทางร้านได้รับ Order ของคุณแล้ว
-              </Card>
-            </Col>
-            <Col>
-              <Card body>
-                กำลังจัดเตรียมอาหารของคุณ
-              </Card>
-            </Col>
-            <Col>
-              <Card body>อาหารของคุณพร้อมส่งถึงคุณ</Card>
-            </Col>
-            <Col>
-              <Card body>อาหารกำลังไปส่งถึงคุณ</Card>
-            </Col>
-            <Col>
-              <Card body>คุณได้รับอาหารแล้ว</Card>
-            </Col>
-          </Row>
+      <div>
+        <MenuSelect />
+        <div className="container-fluid">
+          <div className="text_status">สถานะการจัดส่ง</div>
+          <div className="from_status">
+            <Row>
+              <Col>
+                <Card body className="border border-warning rounded">
+                  <div>
+                    <img className="img_menu img-fluid" src={img_menu} />
+                  </div>
+                  <div className="font_status">ร้านได้รับรายการอาหารแล้ว</div>
+                </Card>
+              </Col>
+              <Col>
+                <Card body className="border border-warning rounded">
+                  <div>
+                    <img className="img_meal img-fluid" src={img_meal} />
+                  </div>
+                  <div className="font_status">กำลังเตรียมอาหารแล้ว</div>
+                </Card>
+              </Col>
+              <Col>
+                <Card body className="border border-warning rounded">
+                  <div>
+                    <img className="img_product img-fluid" src={img_product} />
+                  </div>
+                  <div className="font_status">อาหารของคุณพร้อมส่งถึงคุณ</div>
+                </Card>
+              </Col>
+              <Col>
+                <Card body className="border border-warning rounded">
+                  <div>
+                    <img
+                      className="img_delivery img-fluid"
+                      src={img_delivery}
+                    />
+                  </div>
+                  <div className="font_status">อาหารกำลังไปส่งถึงคุณ</div>
+                </Card>
+              </Col>
+              <Col>
+                <Card body className="border border-warning rounded">
+                  <div>
+                    <img
+                      className="img_breakfast img-fluid"
+                      src={img_breakfast}
+                    />
+                  </div>
+                  <div className="font_status">คุณได้รับอาหารแล้ว</div>
+                </Card>
+              </Col>
+            </Row>
+          </div>
         </div>
       </div>
     );
