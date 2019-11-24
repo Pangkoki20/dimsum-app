2782cf69bd07e6f65b7a5f33193871dbf83e1356
“Pangkoki20” <sasithorn.pangkoki@gmail.com>
2019-11-25 04:42:54 +0700

    Menu Order

--- src/component/MemuOrder/MenuOrder.js
+++ src/component/MemuOrder/MenuOrder.js
@@ -6,7 +6,8 @@ import {
   CardText,
   CardFooter,
   Row,
-  Col
+  Col,
+  Button
 } from "reactstrap";
 import "./MenuOrder.css";
 
@@ -16,36 +17,50 @@ class MenuOrder extends Component {
       <div>
         <div class="container-fluid">
           <div className="text_orderkitchen">รายการอาหารที่เข้ามา</div>
-          <div class="row">
-            <div class="col-12 col-md-8">
-              <Card className="card_orderkitchen">
-                <CardHeader>
-                  <Row>
-                    <Col>Order No.</Col>
-                  </Row>
-                </CardHeader>
-                <CardBody>
-                  <Row>
-                    <Col>1
-                    </Col>
-                    <Col xs="6" sm="4">
-                      .col
-                    </Col>
-                  </Row>
-                </CardBody>
-                <CardFooter>
-                  <div class="row">
-                    <Col>จำนวนรายการอาหาร</Col>
-                    <Col xs="3">------</Col>
-                    <div class="col col-lg-2">รายการ</div>
-                  </div>
-                </CardFooter>
-              </Card>
-              <br></br>
-              <br></br>
-              <br></br>
-            </div>
-            <div class="col-6 col-md-4 ">
+          {/* <div class="row">
+            <div class="col-12 col-md-8"> */}
+          <Card className="card_orderkitchen">
+            <CardHeader>
+              <Row>
+                <Col xs="1">ลำดับ</Col>
+                <Col>รายการ</Col>
+                <Col xs="5">สถานะ</Col>
+              </Row>
+            </CardHeader>
+            <CardBody>
+              <Row>
+                <Col xs="1">1</Col>
+                <Col></Col>
+                <Col xs="auto" className="bt_received">
+                  <Button outline color="success">
+                    ได้รับรายการอาหารแล้ว
+                  </Button>
+                </Col>
+                <Col xs="auto" className="bt_cooking">
+                  <Button outline color="success">
+                    กำลังเตรียมอาหาร
+                  </Button>
+                </Col>
+                <Col xs="auto" className="bt_cooking">
+                  <Button outline color="success">
+                    สำเร็จ
+                  </Button>
+                </Col>
+              </Row>
+            </CardBody>
+            <CardFooter>
+              <div class="row">
+                <Col>จำนวนรายการอาหาร</Col>
+                <Col xs="3">------</Col>
+                <div class="col col-lg-2">รายการ</div>
+              </div>
+            </CardFooter>
+          </Card>
+          <br></br>
+          <br></br>
+          <br></br>
+        </div>
+        {/* <div class="col-6 col-md-4 ">
               <div class="from_orderkitchen">
                 <Card>
                   <CardBody>
@@ -68,10 +83,10 @@ class MenuOrder extends Component {
                   </CardBody>
                 </Card>
               </div>
-            </div>
-          </div>
-        </div>
+            </div> */}
       </div>
+      //   </div>
+      // </div>
     );
   }
 }
