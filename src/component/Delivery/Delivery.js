26f89ef97d7a1ffd36a3c8b2ff29283940ae2b7c
Pumin Swangjang <fvck3oy@hotmail.com>
2020-03-20 14:21:46 +0700

    update

--- src/component/Delivery/Delivery.js
+++ src/component/Delivery/Delivery.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Card, Row, Col } from "reactstrap";
+import { Card, Row, Col, Container } from "reactstrap";
 import "./Delivery.css";
 import MenuSelect from "../MenuSelect/MenuSelect";
 import img_menu from "../../imgdelivery/menu.png";
@@ -9,9 +9,11 @@ import img_delivery from "../../imgdelivery/delivery.png";
 import img_breakfast from "../../imgdelivery/breakfast.png";
 import img_foodorder from "../../imgdelivery/dish.png";
 import axios from "axios";
+import auth from "../service/index"
 class Delivery extends Component {
   state = {
-    user: null
+    user: null,
+    allData: []
   };
 
   onUserChanged = user => {
@@ -39,7 +41,24 @@ class Delivery extends Component {
     } else {
       window.location.href = "/login";
     }
+
+    let user = auth.getToken()
+    let userDecoded = auth.decodeToken(user)
+    let userId = userDecoded.id
+    let userFirstName = userDecoded.firstname
+    let userLastName = userDecoded.lastname
+    let userRole = userDecoded.role
+    this.getAllOrder(userId)
   };
+
+  async getAllOrder(userId) {
+    await axios.get(`http://localhost:3001/api/order/orderbyuser/${userId}`).then(res => {
+      const { data } = res.data;
+      console.log("res : ", res.data);
+      this.setState({ allData: res.data })
+    });
+  }
+
   render() {
     return (
       <div>
@@ -106,6 +125,20 @@ class Delivery extends Component {
                 </Card>
               </Col>
             </Row>
+            <Container className="p-4">
+              <Row>All Orders
+                
+                  {this.state.allData.map(e => {
+                   
+                    return ( <Col md={12}><div className="p-3">order no.{e.id},  status : {e.status} {e.allMenu.map((e,i=0)=>{
+                      return (<div>{i+1}.{e.namefood}</div>)
+                    })}</div></Col>)
+                  }
+                  
+                  )}
+
+              </Row>
+            </Container>
           </div>
         </div>
       </div>
